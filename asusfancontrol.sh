ef452c314186a32b011fa199892d69d40655c9dd
MuxaL <novikoff.mika2.novikov@gmail.com>
2024-10-27 07:09:10 +0800

    Changed README, removed some possible vulnerabilities in asusfancontrol.sh, added some comments to the script.

--- asusfancontrol.sh
+++ asusfancontrol.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# my English is very bad, sorry me
-#Asus zenbook duo UX482 bios 318
 
-#  0 - full speed
-#  2 - auto / silent. I dont know
-#  1 - dont working. Why? I dont know XD
+# This script adjusts the fan speed for some Asus Laptops.
+# 
+# Fan speed options:
+#  0 - Full speed
+#  2 - Auto/Silent (unknown behavior)
+#  Rest - Unknown behavior (don't work?)
+
+# Check if any parameters are passed
+if [ "$#" -ne 0 ]; then
+    echo -e "\033[1;31mWarning: This script does not accept parameters.\033[0m"
+fi
 
 set_fan_speed() {
+    local file_path=$1
     local speed=$2
-    echo "$speed" > "$1"
+    echo "$speed" > "$file_path"
 }
 
 hwmon_number=""
@@ -16,20 +23,25 @@ for entry in /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]*/; do
     hwmon_number="${entry%/}"
 done
 
+# If hwmon number is found, adjust fan speed
 if [[ -n "$hwmon_number" ]]; then
     hwmon_number="${hwmon_number##*hwmon}"
-    
     full_path_to_real_hwmon="/sys/devices/platform/asus-nb-wmi/hwmon/hwmon${hwmon_number}/pwm1_enable"
-    sudo chmod -R 777 $full_path_to_real_hwmon
-    
-    read -r mode_fans < $full_path_to_real_hwmon
 
-    if [ "$mode_fans" -eq 2 ]; then
-        set_fan_speed "$full_path_to_real_hwmon" 0
+    # Change permissions and read the current fan mode
+    if sudo chmod 644 "$full_path_to_real_hwmon"; then
+        read -r mode_fans < "$full_path_to_real_hwmon"
+
+        if [ "$mode_fans" -eq 2 ]; then
+            set_fan_speed "$full_path_to_real_hwmon" 0
+            echo "Fan speed set to full speed (0)."
+        else
+            set_fan_speed "$full_path_to_real_hwmon" 2
+            echo "Fan speed set to silent/auto (2)."
+        fi
     else
-        set_fan_speed "$full_path_to_real_hwmon" 2
+        echo -e "\033[1;31mFailed to change permissions for $full_path_to_real_hwmon.\033[0m\nThis script must be run with root permissions."
     fi
-
 else
-    echo "Nothing found."
+    echo "No hwmon device found."
 fi
